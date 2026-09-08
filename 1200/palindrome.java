ef730d129c4109e140be3d4fdce8209a2c87d0ed
Akash <akashchaurasia1914@gmail.com>
2026-09-08 19:41:32 +0530

    CF 1200

---
+++ 1200/palindrome.java
@@ -0,0 +1,124 @@
+
+import java.io.*;
+
+public class palindrome {
+
+    // ---------- FAST INPUT ----------
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
+
+        private int read() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len == -1) return -1;
+            }
+            return buffer[ptr++];
+        }
+
+        int nextInt() throws IOException {
+            int c;
+
+            do {
+                c = read();
+            } while (c <= ' ');
+
+            int sign = 1;
+
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            int num = 0;
+
+            while (c > ' ') {
+                num = num * 10 + (c - '0');
+                c = read();
+            }
+
+            return num * sign;
+        }
+
+        long nextLong() throws IOException {
+            int c;
+
+            do {
+                c = read();
+            } while (c <= ' ');
+
+            int sign = 1;
+
+            if (c == '-') {
+                sign = -1;
+                c = read();
+            }
+
+            long num = 0;
+
+            while (c > ' ') {
+                num = num * 10 + (c - '0');
+                c = read();
+            }
+
+            return num * sign;
+        }
+
+        String next() throws IOException {
+            int c;
+
+            do {
+                c = read();
+            } while (c <= ' ');
+
+            StringBuilder sb = new StringBuilder();
+
+            while (c > ' ') {
+                sb.append((char) c);
+                c = read();
+            }
+
+            return sb.toString();
+        }
+    }
+
+    // ---------- MAIN ----------
+    public static void main(String[] args) throws Exception {
+
+        FastScanner fs = new FastScanner();
+        StringBuilder out = new StringBuilder();
+
+        int t = fs.nextInt();
+
+        while (t-- > 0) {
+
+            int n = fs.nextInt();
+            String s = fs.next();
+            int cntOne = 0;
+            for (int i = 0; i < n; i++){
+                if(s.charAt(i) == '1'){
+                    cntOne++;
+                }
+            }
+
+            int cntZero = n - cntOne;
+
+            if(cntZero == 1){
+                out.append("BOB\n");
+            }
+
+            else if((cntZero & 1) == 1){
+                out.append("ALICE\n");
+
+            }
+
+            else{
+                out.append("BOB\n");
+            }
+        }
+
+        System.out.print(out);
+    }
+}
